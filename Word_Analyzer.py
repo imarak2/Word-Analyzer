71773ad9df7dd404ca31af0f10964d362ea6b19f
imarak2 <80424438+imarak2@users.noreply.github.com>
2026-01-02 14:51:35 +0300

    Added comments to source code make reading it easier.
    Added comments to help a user better understand how the code works.

--- Word_Analyzer.py
+++ Word_Analyzer.py
@@ -1,5 +1,5 @@
 import string
-
+# Welcoming the user
 print("Welcome to the Word Analyzer!")
 total_text = input("Type in here to test it out!: ")
 
@@ -8,7 +8,7 @@ digits = 0
 spaces = 0
 lowercase_letters = 0
 uppercase_letters = 0
-
+# This part of the code checks if the text given has any punctuation, digits, lowercase and uppercase letters, and spaces, and if it does, the variables for them will go up by one each time 
 for char in total_text:
     if char in string.punctuation:
         punctuation += 1
@@ -21,7 +21,9 @@ for char in total_text:
     elif char in string.whitespace:
         spaces += 1
 
+# This is the sanity check
 full_total = punctuation + digits + lowercase_letters + uppercase_letters + spaces
+
 
 if full_total == len(total_text):
     print("Everything looks good! The result is correct! ✅")
@@ -32,7 +34,7 @@ else:
         exec(open("Word_Analyzer.py").read())
     else:
         print("Thank you for using the Word Analyzer. Goodbye!")
-
+# Prints the results
 print(" ")
 
 print(
@@ -43,4 +45,5 @@ print(
     f"Uppercase letters: {uppercase_letters}\n"
     f"Spaces: {spaces}"
 )
+
 input("Press Enter to exit the program.")
